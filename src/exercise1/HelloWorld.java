f85222b8b7d069811fc1c5a4b879b871b2519554
ivanats <ivana.tsankova1@gmail.com>
2020-09-30 14:33:17 +0000

    exercise

--- src/exercise1/HelloWorld.java
+++ src/exercise1/HelloWorld.java
@@ -2,6 +2,6 @@ package exercise1;
 
     public class HelloWorld {
       public static void main ( String [] args) {
-        System.out.print ( "Hello World");
+        System.out.print ( "Hello World!");
       }
     }
